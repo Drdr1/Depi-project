3468c8f53355b0bd8975b433365d1b396c0f3220
ahmed darder <91543865+Drdr1@users.noreply.github.com>
2024-10-01 01:16:13 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def Hello_DevOps():
-    return 'Hello, DevOps!' 
+    return 'Hello, DevOps-dr!' 
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
